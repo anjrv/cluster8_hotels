d4427086acb2c4140e4d50e53bb6e69c67f4b8be
Anna Jaerving <anjrv@machine.localdomain>
2021-02-23 12:11:33 +0000

    reservation

--- Review.java
+++ Review.java
@@ -1,5 +1,7 @@
 public class Review {
     private final int grade;
+    private final int rnumber;
+    private final String hname;
     private final String text;
     private final String reservationID;
 
