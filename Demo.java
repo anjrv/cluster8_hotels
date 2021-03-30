762ec587b3acd9c0a36ce1724dec920139de0ba6
Anna Jaerving <annajrving@gmail.com>
2021-03-30 10:41:15 +0000

    scanner frame

--- Demo.java
+++ Demo.java
@@ -1,81 +1,129 @@
 import java.util.ArrayList;
 import java.util.Hashtable;
-import hotels.Logic;
-import hotels.Hotel;
-import hotels.Room;
+import java.util.Scanner;
+
+import hotels.*;
 
 /**
- * Provides an example how to create a business logic item which can be used to
- * forward queries through the hotel package.
+ * Demo command line interface that shows basic functionality of the hotel
+ * business logic layer
  * 
  * @author: Einar Jónsson, Eydís Sylvía Einarsdóttir, Jaan Jaerving, Snorri
  *          Steinn Stefánsson Thors
  */
 public class Demo {
+    private static final Scanner s = new Scanner(System.in);
+    private static final Logic l = new Logic();
 
-    public static void setup() {
+    public static void hotels() {
 
     }
 
-    /**
-     * In this case we query for hotels and iterate through the resulting ArrayList
-     * where we use the Hotel.getName() method to print out the name of each
-     * individual hotel.
-     * 
-     * @param args not used
-     */
+    public static void rooms() {
+
+    }
+
+    public static void reservations() {
+
+    }
+
+    public static void reviews() {
+
+    }
+
+    public static void update() {
+        boolean cont = true;
+        System.out.println();
+        System.out.println("What would you like to update?");
+        System.out.println("1) Reservation");
+        System.out.println("2) Review");
+        System.out.println("Type return to go back.");
+
+        while (cont && s.hasNext()) {
+            String response = s.next();
+
+            switch (response) {
+            case "1":
+                hotels();
+            case "2":
+                rooms();
+            case "return":
+                cont = false;
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
+            }
+        }
+        return;
+    }
+
+    public static void query() {
+        boolean cont = true;
+        System.out.println();
+        System.out.println("What would you like to query?");
+        System.out.println("1) Hotels");
+        System.out.println("2) Rooms");
+        System.out.println("3) Reservations");
+        System.out.println("4) Reviews");
+        System.out.println("Type return to go back.");
+
+        while (cont && s.hasNext()) {
+            String response = s.next();
+
+            switch (response) {
+            case "1":
+                hotels();
+            case "2":
+                rooms();
+            case "3":
+                reservations();
+            case "4":
+                reviews();
+            case "return":
+                cont = false;
+                break;
+            default:
+                System.out.println("Invalid input");
+                break;
+            }
+        }
+        return;
+    }
+
+    public static void intro() {
+        System.out.println();
+        System.out.println("What would you like to do?");
+        System.out.println("1) Query");
+        System.out.println("2) Update");
+        System.out.println("Type quit to stop.");
+
+        while (s.hasNext()) {
+            String response = s.next();
+
+            switch (response) {
+            case "1":
+                query();
+                break;
+            case "2":
+                update();
+                break;
+            case "quit":
+                System.exit(0);
+            default:
+                System.out.println("Invalid input");
+                break;
+            }
+
+            System.out.println();
+            System.out.println("What would you like to do?");
+            System.out.println("1) Query");
+            System.out.println("2) Update");
+            System.out.println("Type quit to stop.");
+        }
+    }
+
     public static void main(String[] args) {
-        Logic bn = new Logic();
-
-        String[] validHotelParams = bn.getHotelParams();
-        System.out.println("Valid parameters to query hotels are:");
-        for (int i = 0; i < validHotelParams.length; i++) {
-            System.out.println(validHotelParams[i]);
-        }
-
-        try {
-            Hashtable<String, String> hotelParams = new Hashtable<String, String>();
-            hotelParams.put("Region", "0"); // Query for region 1 only.
-
-            ArrayList<Hotel> hotels = bn.getHotels(hotelParams);
-            // If working with Review, Room or Reservation objects
-            // those Object classes need to be imported in the same
-            // way that the Hotel object has been at the top of this
-            // file
-
-            System.out.println("-------------------------------------------------------");
-            System.out.println("Query based on region 0 returns the following hotels:");
-            for (int i = 0; i < hotels.size(); i++) {
-                System.out.println(hotels.get(i).getName());
-            }
-
-            System.out.println("-------------------------------------------------------");
-            System.out.println("Query of rooms based on hotel result returns the rooms:");
-            for (int i = 0; i < hotels.size(); i++) {
-                ArrayList<Room> rooms = hotels.get(i).getRooms();
-                for (int j = 0; j < rooms.size(); j++) {
-                    System.out.println(rooms.get(j).getRnumber());
-                }
-            }
-
-            Hashtable<String, String> roomParams = new Hashtable<String, String>();
-            roomParams.put("wifi","1");
-            roomParams.put("adults","1");
-            roomParams.put("price","31000");
-
-            ArrayList<Room> rooms2 = bn.getRooms(roomParams);
-
-            System.out.println("-------------------------------------------------------");
-            System.out.println("Query of rooms based on wifi and 1 adult:");
-            for (int i = 0; i < rooms2.size(); i++) {
-                System.out.println(rooms2.get(i).getRnumber());
-            }
-
-
-
-
-        } catch (Exception err) {
-            System.err.println("Demo error: " + err);
-        }
+        intro();
     }
 }
